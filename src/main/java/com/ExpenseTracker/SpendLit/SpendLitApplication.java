749b520c322c9998172e5b2dd1f5cb0cc2fcbb27
Angelina Mathew <angelinamathew@Angelinas-MacBook-Pro.local>
2024-12-22 18:21:32 -0500

    first commit

---
+++ src/main/java/com/ExpenseTracker/SpendLit/SpendLitApplication.java
@@ -0,0 +1,13 @@
+package com.ExpenseTracker.SpendLit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendLitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendLitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ExpenseTracker/SpendLit/SpendLitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ExpenseTracker.SpendLit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendLitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
