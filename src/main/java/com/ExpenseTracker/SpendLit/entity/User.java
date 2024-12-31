48f962b5cd654ff626a15e39e957cd375acb2256
Angelinamathew <angelinamathew707@gmail.com>
2024-12-31 00:32:10 -0500

    user class implemented UserDetails

--- src/main/java/com/ExpenseTracker/SpendLit/entity/User.java
+++ src/main/java/com/ExpenseTracker/SpendLit/entity/User.java
@@ -2,11 +2,17 @@ package com.ExpenseTracker.SpendLit.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,4 +28,36 @@ public class User {
 
     private Role role;
 
+    // It returns a collection of GrantedAuthority objects, which represent the roles or permissions assigned to the user.
+    // Here, the user's role is converted to a SimpleGrantedAuthority using the role's name() method.
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+//        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+      return true;
+    }
 }
